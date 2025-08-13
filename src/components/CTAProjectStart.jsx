88137d1e8ff9438154495e9cdab5f1b36244cd56
Franco-Cipolla <franchito2008@icloud.com>
2025-08-13 10:17:00 +0200

    `Refactored Contact and Erstgespraech components to match new formspark import and usage`

--- src/components/CTAProjectStart.jsx
+++ src/components/CTAProjectStart.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const CTAProjectStart = ({ text = "Projekt starten" }) => {
+const CTAProjectStart = ({ text = "Projekt starten" ,onClick}) => {
   const navigate = useNavigate()
+  const handleClick = () => {
+    if (onClick) onClick() // Menü schließen, falls nötig
+    navigate('/erstgespraech') // hier zur Erstgespräch-Seite navigieren
+  }
 
   return (
     <button
-      onClick={() => navigate('/', { state: { scrollTo: 'contact' } })}
+      onClick={handleClick}
       className="px-6 py-3 text-lg font-semibold tracking-wide text-[#5A3E4F] bg-gradient-to-br cursor-pointer from-[#EAEEE3] via-[#EFCFDA] to-[#E1C8D3] border-2 border-white  rounded hover:opacity-90 transition duration-300 ease-in-out shadow-md transform hover:-translate-y-1"
     >
       {text}
