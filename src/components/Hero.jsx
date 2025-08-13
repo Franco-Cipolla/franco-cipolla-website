6848199d8fa8d8b14e3dcf6b5aa88bd4cfb89b69
Franco-Cipolla <franchito2008@icloud.com>
2025-08-13 13:49:23 +0200

    `Update Hero component CSS classes for left and right circle elements`

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -116,11 +116,11 @@ const Hero = () => {
       <div className="relative overflow-hidden">
         <span
           ref={leftCircleRef}
-          className="fixed will-change-transform w-[300px] h-[300px] blur-[80px] opacity-50 sm:w-[350px] sm:h-[350px] sm:blur-[100px] md:w-[400px] md:h-[400px] md:blur-[100px] md:opacity-70 bottom-[-100px] left-[-35px] rounded-full bg-gradient-to-tr from-[#003566] via-[#001D3D] to-[#00A6FB] z-[-1]"
+          className="fixed will-change-transform w-[300px] h-[300px] blur-[80px] opacity-60 sm:w-[350px] sm:h-[350px] sm:blur-[100px] md:w-[400px] md:h-[400px] md:blur-[100px]  bottom-[-100px] left-[-35px] rounded-full bg-gradient-to-tr from-[#003566] via-[#001D3D] to-[#00A6FB] z-[-1]"
         />
         <span
           ref={rightCircleRef}
-          className="fixed will-change-transform w-[250px] h-[250px] blur-[60px] opacity-50 sm:w-[300px] sm:h-[300px] sm:blur-[80px] sm:opacity-70 md:w-[400px] md:h-[400px] md:blur-[100px] md:opacity-70 top-[40px] right-[-90px] rounded-full bg-gradient-to-bl from-[#003566] via-[#001D3D] to-[#00A6FB] z-[-1]"
+          className="fixed will-change-transform w-[250px] h-[250px] blur-[60px] opacity-60 sm:w-[300px] sm:h-[300px] sm:blur-[80px] md:w-[400px] md:h-[400px] md:blur-[100px]  top-[40px] right-[-90px] rounded-full bg-gradient-to-bl from-[#003566] via-[#001D3D] to-[#00A6FB] z-[-1]"
         />
       </div>
 
